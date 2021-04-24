d12cfd9636ced4c9b935e5ce7ddcbb4ee053d5e5
hpnrep6 <hpnrep6@gmail.com>
2021-04-24 12:10:03 -0400

    initiawl

--- public/product/product.js
+++ public/product/product.js
@@ -1,1 +1,13 @@
-console.log(43)
+var b2 = document.getElementById('2d');
+var b3 = document.getElementById('3d');
+
+var f2 = document.getElementById('image');
+var f3 = document.getElementById('model');
+
+b2.addEventListener('click', (e) => {
+    f2.style.visibility = 'visible'
+})
+
+b3.addEventListener('click', (e) => {
+    f2.style.visibility = 'hidden'
+})
